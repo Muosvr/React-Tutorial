7b710525956529cf0cd1c1d863d3b47501b934cb
Jason Wu <lujason2015@gmail.com>
2018-06-10 20:58:03 -0400

    intro

---
+++ Introduction.js
@@ -0,0 +1,12 @@
+/*
+What is React?
+
+- JavaScript framework for creating user interfaces
+    - Full single page web applications
+    - Just certain parts of a website (like a search form)
+
+- Component-based
+- Very fast (thanks to the virtual DOM)
+- Created by Facebook
+
+*/
